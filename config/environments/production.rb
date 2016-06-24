2e937e6b58757752a169c6058126a507e8223931
Sandy Hogan <SVern99@aol.com>
2016-06-24 20:08:50 +0000

    added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Added as per Devise installation instructions
+  config.action_mailer.default_url_options = { host: 'https://dashboard.heroku.com/apps/sandypinterested' }
 end
